05d466a3c73b812dcfec346f36f5b285845b7852
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-08 09:34:02 -0300

    feat:  export functionality CSV & PDF

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const wizardRoutes = require("./src/routes/wizardRoutes");
-//const houseRoutes = require("./src/routes/houseRoutes");
+const wizardRoutes = require("./src/routes/wizardRoutes.js");
+const houseRoutes = require("./src/routes/houseRoutes.js");
+const reportRoutes = require("./src/routes/reportRoutes.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", wizardRoutes);
-//app.use("/api", houseRoutes);
+app.use("/api", wizardRoutes); 
+app.use("/api", houseRoutes); 
+app.use("/api", reportRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+require('dotenv').config(); 
 
-const pool = new Pool ({
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
