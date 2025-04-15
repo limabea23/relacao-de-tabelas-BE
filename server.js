01ac0ec3e5fb28d8be6509a4d4e64bb56029ee57
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-15 15:54:11 -0300

    refactor: update server.js

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const cors = require("cors");
 const wizardRoutes = require("./src/routes/wizardRoutes.js");
 const houseRoutes = require("./src/routes/houseRoutes.js");
 const reportRoutes = require("./src/routes/reportRoutes.js");
+const setupSwagger = require('./src/config/swagger');
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+setupSwagger(app);
 
 app.use("/api", wizardRoutes); 
 app.use("/api", houseRoutes); 
 app.use("/api", reportRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
